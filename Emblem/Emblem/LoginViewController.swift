58c7b996852b5e449915c203960d1a8c3bead127
Dane Jordan <dane.e.jordan@gmail.com>
2016-08-03 20:48:05 -0700

    first commit

---
+++ Emblem/Emblem/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  Emblem
+//
+//  Created by Dane Jordan on 8/3/16.
+//  Copyright © 2016 Hadashco. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
