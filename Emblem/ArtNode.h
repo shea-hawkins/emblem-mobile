cd0cca50ddab90372d268a568a19d793252da970
Shea Hawkins <shea.m.hawkins@gmail.com>
2016-08-16 16:16:42 -0700

    Cleanup code base (iteration 1)

---
+++ Emblem/ArtNode.h
@@ -0,0 +1,21 @@
+//
+//  ArtNode.h
+//  Emblem
+//
+//  Created by Humanity on 8/16/16.
+//  Copyright © 2016 Hadashco. All rights reserved.
+//
+
+#ifndef ArtNode_h
+#define ArtNode_h
+
+#import <SceneKit/SceneKit.h>
+
+@interface ArtNode : NSObject
+
+@property(nonatomic, readonly) SCNNode* node;
+
+@end
+
+
+#endif /* ArtNode_h */
