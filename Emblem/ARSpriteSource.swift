e4f3de617a5b1bb4efc36a590c33473661074909
Shea Hawkins <shea.m.hawkins@gmail.com>
2016-08-19 16:56:07 -0700

    Implement event system on MenuSpriteScene

--- Emblem/ARSpriteSource.swift
+++ Emblem/ARSpriteSource.swift
@@ -5,5 +5,44 @@
 //  Created by Humanity on 8/17/16.
 //  Copyright © 2016 Hadashco. All rights reserved.
 //
+//import UIKit
+import SpriteKit
 
-import Foundation
+class MenuSpriteScene: SKScene {
+    var upvoteNode: SKSpriteNode!
+    var downvoteNode: SKSpriteNode!
+    var events: [String: Array<() -> Void>] = [String: Array<() -> Void>]()
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch = touches.first as UITouch!
+        let location = touch?.locationInNode(self)
+        if self.upvoteNode.containsPoint(location!) {
+            self.emit("upvote")
+        }
+        
+        if self.downvoteNode.containsPoint(location!) {
+            self.emit("downvote")
+        }
+    }
+    
+    func emit(eventName: String) {
+        if let listeners = events[eventName] {
+            listeners.forEach({(body) -> Void in
+                body()
+            });
+        }
+    }
+    
+    func on(eventName: String, callback: () -> Void) {
+        if var eventArray = events[eventName] {
+            eventArray.append(callback)
+        } else {
+            events[eventName] = [callback]
+        }
+    }
+}
