a4827956be5de20c5903f808ff4b918c4607f7a2
Shea Hawkins <shea.m.hawkins@gmail.com>
2016-08-19 19:47:33 -0700

    Add Sprite Menu (WIP)

--- Emblem/ARSpriteSource.swift
+++ Emblem/ARSpriteSource.swift
@@ -13,6 +13,28 @@ class MenuSpriteScene: SKScene {
     var downvoteNode: SKSpriteNode!
     var events: [String: Array<() -> Void>] = [String: Array<() -> Void>]()
     
+    override init(size: CGSize) {
+        super.init(size: size)
+        
+        self.backgroundColor = UIColor.redColor()
+        size.w
+        let spriteSize = size.width/12
+        self.upvoteNode = SKSpriteNode(imageNamed: "upvote.png");
+        
+        self.downvoteNode = SKSpriteNode(imageNamed "downvote.png");
+        
+        
+        self.pauseNode = SKSpriteNode(imageNamed: "Pause Button")
+        
+    }
+    
+    func createMenuSprite(imageName: String, spriteSize: CGSize, position: CGPoint) -> SKSpriteNode {
+        let node = SKSpriteNode(imageNamed: imageName)
+        node.size = spriteSize
+        node.position = CGPoint(x: spriteSize + , y: <#T##CGFloat#>)
+        
+    }
+    
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
         let touch = touches.first as UITouch!
         let location = touch?.locationInNode(self)
